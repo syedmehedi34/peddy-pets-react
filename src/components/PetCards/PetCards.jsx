61ecf72a39d3d1787ea0cbf21227fafb89ad36c1
syedmehedi34 <syedmeehdi34@gmail.com>
2024-10-22 21:40:19 +0600

    spinner added

--- src/components/PetCards/PetCards.jsx
+++ src/components/PetCards/PetCards.jsx
@@ -5,19 +5,37 @@ import PetCard from "../PetCard/PetCard";
 const PetCards = ({ handleSelectPet, searchedPet }) => {
   // console.log(handlePetsData);
   const [pets, setPets] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     if (searchedPet) {
       fetch(
         `https://openapi.programming-hero.com/api/peddy/category/${searchedPet}`
       )
         .then((res) => res.json())
-        .then((data) => setPets(data.data));
+        .then((data) => {
+          setPets(data.data);
+          setTimeout(function () {
+            setLoading(false);
+          }, 3000);
+        });
     } else {
       fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
         .then((res) => res.json())
-        .then((data) => setPets(data.pets));
+        .then((data) => {
+          setPets(data.pets);
+          setTimeout(function () {
+            setLoading(false);
+          }, 2000);
+        });
     }
   }, [searchedPet]);
+  if (loading) {
+    return (
+      <div className="col-span-8 flex items-center justify-center">
+        <span className="loading loading-bars loading-md"></span>
+      </div>
+    );
+  }
 
   return (
     <div className="h-fit col-span-8">
