79c247ede6eaf8ec84baae199bfab7c93cf1f103
syedmehedi34 <syedmeehdi34@gmail.com>
2024-10-22 22:54:02 +0600

    blank pets showed

--- src/components/PetCards/PetCards.jsx
+++ src/components/PetCards/PetCards.jsx
@@ -3,54 +3,70 @@ import { useEffect, useState } from "react";
 import PetCard from "../PetCard/PetCard";
 
 const PetCards = ({ handleSelectPet, searchedPet }) => {
-  // console.log(handlePetsData);
   const [pets, setPets] = useState([]);
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    setLoading(true);
+
     if (searchedPet) {
       fetch(
         `https://openapi.programming-hero.com/api/peddy/category/${searchedPet}`
       )
         .then((res) => res.json())
         .then((data) => {
-          setPets(data.data);
-          setTimeout(function () {
-            setLoading(false);
-          }, 3000);
+          setPets(data.data); // Set fetched data
+
+          // Wait for 2 seconds (or any duration you want)
+          setTimeout(() => {
+            setLoading(false); // After 2 seconds, hide the spinner
+          }, 500);
         });
     } else {
       fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
         .then((res) => res.json())
         .then((data) => {
-          setPets(data.pets);
-          setTimeout(function () {
-            setLoading(false);
-          }, 2000);
+          setPets(data.pets); // Set fetched data
+
+          // Wait for 2 seconds (or any duration you want)
+          setTimeout(() => {
+            setLoading(false); // After 2 seconds, hide the spinner
+          }, 500);
         });
     }
   }, [searchedPet]);
+
   if (loading) {
     return (
-      <div className="col-span-8 flex items-center justify-center">
+      <div className="col-span-8 flex justify-center">
         <span className="loading loading-bars loading-md"></span>
       </div>
     );
   }
 
-  return (
-    <div className="h-fit col-span-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {pets.map((pet) => (
-          <PetCard
-            key={pet.petId}
-            pet={pet}
-            handleSelectPet={handleSelectPet}
-            searchedPet={searchedPet}
-          ></PetCard>
-        ))}
+  if (pets.length) {
+    console.log(pets.length);
+    return (
+      <div className="h-fit col-span-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          {pets.map((pet) => (
+            <PetCard
+              key={pet.petId}
+              pet={pet}
+              handleSelectPet={handleSelectPet}
+              searchedPet={searchedPet}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="h-fit col-span-8">
+        <h1>Content</h1>
+      </div>
+    );
+  }
 };
 
 export default PetCards;
